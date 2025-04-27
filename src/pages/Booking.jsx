5438466e948210a2ebaa884ba71d7b35f4dc4e63
SERHII.PROTSAK@lnu.edu.ua <SERHII.PROTSAK@lnu.edu.ua>
2025-04-27 19:22:28 +0300

    Implemented Booking page with movie details and CinemaHall

---
+++ src/pages/Booking.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import movies from '../data/movies';
+import CinemaHall from '../components/CinemaHall';
+
+const Booking = () => {
+  const { id } = useParams();
+  const movie = movies.find((m) => m.id === parseInt(id));
+
+  if (!movie) {
+    return <div>Movie not found.</div>;
+  }
+
+  return (
+    <div className="booking-page">
+      <h1>Booking for {movie.title}</h1>
+      <p>Session: {movie.sessionDateTime}</p>
+      <CinemaHall />
+      <Link to="/" className="back-link">
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default Booking;
